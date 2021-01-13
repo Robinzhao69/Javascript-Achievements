f4312c2d7ede06c2ca5676e112cf7739de54571d
Robin <titanen.197@hotmail.com>
2021-01-13 15:31:43 +0100

    done

--- opdrachten/JSB2L3 gimme a break/break.js
+++ opdrachten/JSB2L3 gimme a break/break.js
@@ -1,30 +1,31 @@
-//via het DOM document object model wordt er een link gemaakt naar elementen uit je HTML file
-i = document.getElementById("break_img")//verwijzing naar je <img id='break_img'>
-p = document.getElementById("clock")//verwijzing naar je <p id='clock'>
+image = document.getElementById("break_img");
+clock = document.getElementById("clock");
 
-breakStart = new Date('November 2, 2020 12:00:00');
-breakEnd = new Date('November 2, 2020 12:00:10');
-//stel de begin en eindtijd van je break hier in
+breakStart = new Date();
+breakStart.setSeconds(breakStart.getSeconds() + 10);
+breakEnd = new Date();
+breakEnd.setSeconds(breakStart.getSeconds() + 10);
 
-
-e = setInterval(updateTime,1000) //code zorgt dat de update time functie elke seconde wordt uitgevoerd
+updateTime();
+e = setInterval(updateTime,1000);
 
 function updateTime(){
-    d = new Date('20 11:59:55');
-    
-    
-     p.innerHTML = "Break Time from "+breakStart.toTimeString()+" until " +breakEnd.toTimeString()+"<br/> Time: "+ d.getHours() +" : "+ d.getMinutes() +" : "+ d.getSeconds()
-     
-     
-       
+    d = new Date();
+    clock.innerHTML = "Break Time from "+ breakStart.toTimeString()+" until " +breakEnd.toTimeString()+"<br/> Time: "+d.getHours() +" : "+ d.getMinutes() +" : "+ d.getSeconds();
+
    //check hier hoe laat het is en of je wel of geen break moet hebben
+    if (d > breakStart && d < breakEnd) {
+        breakNow();
+    } else {
+        endBreak();
+    }
 }
 function breakNow(){
     //pas hier de source van je image aan voor als je een break hebt
-    i.src = "break.jpg"
-   
+    image.src = "break.jpg";
+
 }
 function endBreak(){
    //Pas hier de source van je image aan voor als je aan het werk bent
-   i.src = "working-hard.jpg"
+   image.src = "working-hard.jpg";
 }
